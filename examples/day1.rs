bc12ab90fd27682f75077480c46cb117799252c2
Chris Emerson <github@mail.nosreme.org>
2023-12-01 06:52:44 +0000

    Day 1 part 2 done.

--- examples/day1.rs
+++ examples/day1.rs
@@ -19,21 +19,65 @@ fn part1(data: &Data) -> usize {
     }
     sum
 }}
+
 timeit!{
 fn part2(data: &Data) -> usize {
-    unimplemented!()
+    let mut sum = 0usize;
+    let digits = [
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+    for line in data.lines() {
+        let mut vals = Vec::new();
+        for i in 0..line.len() {
+            for (s, v) in &digits {
+                if line[i..].starts_with(s) {
+                    vals.push(v);
+                    break;
+                }
+            }
+        }
+        sum += dbg!(vals[0] * 10 + *vals.last().unwrap());
+    }
+    sum
 }}
 
 #[test]
 fn test() {
-    let tests = r#"1abc2
+    let test1 = r#"1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"#;
-    let data = parse_input(&tests);
+    let test2 = r#"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"#;
 
-    assert_eq!(part1(&data), 142);
-    assert_eq!(part2(&data), 0);
+    let data1 = parse_input(&test1);
+    let data2 = parse_input(&test2);
+
+    assert_eq!(part1(&data1), 142);
+    assert_eq!(part2(&data2), 281);
 }
 
 fn main() -> std::io::Result<()>{
