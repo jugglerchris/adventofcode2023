c4cd5a9064641ea5f621ce4813e9dcbe79e55b12
Chris Emerson <github@mail.nosreme.org>
2023-12-03 06:11:07 +0000

    Day 3 part 1.

--- examples/day3.rs
+++ examples/day3.rs
@@ -1,13 +1,76 @@
 #[allow(unused)]
 use adventofcode2023::{get_input,parse_lines,regex_parser,timeit};
+use std::collections::HashMap;
 
-type Data = ();
+#[derive(Debug, Copy, Clone)]
+struct Symbol(u8);
+
+#[derive(Debug)]
+struct PartInfo {
+    val: usize,
+    x: usize,
+    y: usize,
+    digits: usize,
+}
+
+struct Data {
+    symbols: HashMap<(usize, usize), Symbol>,
+    parts: Vec<PartInfo>,
+}
+
 fn parse_input(input: &str) -> Data {
+    let mut symbols = HashMap::new();
+    let mut parts = Vec::new();
+
+    let mut part = None;
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.as_bytes().iter().cloned().enumerate() {
+            if (x == 0 || !c.is_ascii_digit()) && part.is_some() {
+                parts.push(part.take().unwrap());
+            }
+            match c {
+                b'.' => {}
+                b'0'..=b'9' => {
+                    if part.is_none() {
+                        part = Some(PartInfo {
+                            val: (c - b'0') as usize,
+                            x, y,
+                            digits: 1
+                        });
+                    } else {
+                        let part = dbg!(part.as_mut().unwrap());
+                        dbg!((x, y));
+                        assert!(x == 0 || (part.x == x - part.digits && part.y == y));
+                        part.val = (part.val * 10) + (c - b'0') as usize;
+                        part.digits += 1;
+                    }
+                }
+                c => {
+                    symbols.insert((x, y), Symbol(c));
+                }
+            }
+        }
+    }
+    if let Some(part) = part {
+        parts.push(part);
+    }
+    Data { symbols, parts }
 }
 
 timeit!{
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    let mut result = 0;
+    'parts: for part in &data.parts {
+        for y in part.y.saturating_sub(1)..=part.y+1 {
+            for x in part.x.saturating_sub(1)..=(part.x + part.digits) {
+                if data.symbols.contains_key(&(x, y)) {
+                    result += part.val;
+                    continue 'parts;
+                }
+            }
+        }
+    }
+    result
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
@@ -16,11 +79,20 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
+    let tests = r#"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."#;
     let data = parse_input(&tests);
 
-    assert_eq!(part1(&data), 0);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part1(&data), 4361);
+//    assert_eq!(part2(&data), 0);
 }
 
 fn main() -> std::io::Result<()>{
