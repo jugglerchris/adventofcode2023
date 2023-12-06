64192a248cbfb4ea44ee315d5294776005541c47
Chris Emerson <github@mail.nosreme.org>
2023-12-06 20:29:50 +0000

    Day 6 part 2.

--- examples/day6.rs
+++ examples/day6.rs
@@ -31,6 +31,28 @@ fn parse_input(input: &str) -> Data {
          .collect()
 }
 
+fn parse_input2(input: &str) -> Race {
+    let mut lines = input.lines();
+    let time_str = lines.next()
+                        .unwrap()
+                        .chars()
+                        .filter(|&c| '0' <= c && c <= '9')
+                        .collect::<String>();
+    let dist_str = lines.next()
+                        .unwrap()
+                        .chars()
+                        .filter(|&c| '0' <= c && c <= '9')
+                        .collect::<String>();
+    let time: usize = time_str.parse().unwrap();
+    let distance: usize = dist_str.parse().unwrap();
+
+    assert_eq!(lines.next(), None);
+
+    Race {
+        time, distance
+    }
+}
+
 timeit!{
 fn part1(data: &Data) -> usize {
     let mut result = 1;
@@ -57,8 +79,23 @@ fn part1(data: &Data) -> usize {
     result
 }}
 timeit!{
-fn part2(data: &Data) -> usize {
-    unimplemented!()
+fn part2(race: &Race) -> usize {
+    // t = time
+    // n = length of press
+    // distance = (t - n) * n
+    // When matching the given distance d:
+    // d = (t-n)*n
+    // n^2 - t*n + d = 0
+    let t = race.time as f64;
+    let d = race.distance as f64 + 0.000000001;
+
+    let cross1 = (t - (t*t - 4.0*d).sqrt()) / 2.0;
+    let cross2 = (t + (t*t - 4.0*d).sqrt()) / 2.0;
+    let cross1 = cross1.ceil();
+    let cross2 = cross2.floor();
+
+    let num_ways = (cross2 - cross1) + 1.0;
+    num_ways as usize
 }}
 
 #[test]
@@ -68,7 +105,9 @@ Distance:  9  40  200"#;
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 288);
-    //assert_eq!(part2(&data), 0);
+
+    let data2 = parse_input2(&tests);
+    assert_eq!(part2(&data2), 71503);
 }
 
 fn main() -> std::io::Result<()>{
@@ -79,8 +118,10 @@ fn main() -> std::io::Result<()>{
     // Part 1
     println!("{}", part1(&data));
 
+    let data2 = parse_input2(&input);
+
     // Part 2
-    println!("{}", part2(&data));
+    println!("{}", part2(&data2));
 
     Ok(())
 }
