ca7b73a1a2722addddf194e6e913d5c250698ff2
Chris Emerson <github@mail.nosreme.org>
2023-12-07 08:02:06 +0000

    Day 7 part 2.

--- examples/day7.rs
+++ examples/day7.rs
@@ -94,6 +94,39 @@ fn calc_hand_value(cards: &[u8; 5]) -> HandValue {
     }
 }
 
+fn calc_hand_value_2(cards: &[u8; 5]) -> HandValue {
+    let mut counts = [0u8; 256];
+
+    for &v in cards {
+        counts[v as usize] += 1;
+    }
+
+    // Keep the jokers back
+    let joker_index = card_value('J') as usize;
+    let num_jokers = counts[joker_index];
+    counts[joker_index] = 0;
+
+    let mut countv = counts.into_iter()
+                           .filter(|&c| c>0)
+                           .collect::<Vec<_>>();
+    countv.sort();
+    if countv.len() == 0 {
+        countv.push(num_jokers);
+    } else {
+        *countv.last_mut().unwrap() += num_jokers;
+    }
+    match &countv[..] {
+        &[5] => HandValue::Five,
+        &[1, 4] => HandValue::Four,
+        &[2, 3] => HandValue::FullHouse,
+        &[1, 1, 3] => HandValue::Three,
+        &[1, 2, 2] => HandValue::TwoPair,
+        &[1, 1, 1, 2] => HandValue::Pair,
+        &[1, 1, 1, 1, 1] => HandValue::HighCard,
+        _ => panic!("Invalid hand: {:?}", &countv),
+    }
+}
+
 timeit!{
 fn part1(data: &Data) -> usize {
     let mut data = data.iter().cloned().collect::<Vec<_>>();
@@ -107,7 +140,26 @@ fn part1(data: &Data) -> usize {
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
-    unimplemented!()
+    let mut data = data.iter().cloned().collect::<Vec<_>>();
+
+    // Update the hand values
+    for hand in &mut data {
+        // Update the hand value
+        hand.hand_value = calc_hand_value_2(&hand.cards);
+        // Update Joker values
+        for v in &mut hand.cards {
+            if *v == 11 {
+                *v = 0;
+            }
+        }
+    }
+    data.sort();
+    data.into_iter()
+        .enumerate()
+        .map(|(i, hand)| {
+            (i+1) * hand.bid
+        })
+        .sum()
 }}
 
 #[test]
@@ -120,7 +172,7 @@ QQQJA 483"#;
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 6440);
-//    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 5905);
 }
 
 fn main() -> std::io::Result<()>{
