b70b61b461b6f78247f673e66d791b1fc3490484
Chris Emerson <github@mail.nosreme.org>
2023-12-13 07:00:22 +0000

    Day 13 part 2.

--- examples/day13.rs
+++ examples/day13.rs
@@ -8,6 +8,12 @@ struct Field {
     rows: Vec<u32>,
 }
 
+fn nearly_eq(a: u32, b: u32) -> bool {
+    let diffs = a ^ b;
+    let ones = diffs.count_ones();
+    ones <= 1
+}
+
 impl Field {
     pub fn col(&self, i: usize) -> u32 {
         let mut result = 0;
@@ -45,6 +51,63 @@ impl Field {
                         return Some(i+1);
                     }
                     if self.rows[i-offs] != self.rows[i+offs+1] {
+                        continue 'i;
+                    }
+                }
+            }
+        }
+        None
+    }
+    pub fn vert_sym_smudged(&self) -> Option<usize> {
+        'i: for i in 0..self.num_cols {
+            if nearly_eq(self.col(i), self.col(i+1)) {
+                let mut smudges = 0;
+                for offs in 0.. {
+                    if offs > i {
+                        if smudges == 1 {
+                            return Some(self.num_cols - i - 1);
+                        } else {
+                            continue 'i;
+                        }
+                    } else if i+offs+1 >= self.num_cols {
+                        if smudges == 1 {
+                            return Some(self.num_cols - i - 1);
+                        } else {
+                            continue 'i;
+                        }
+                    }
+                    let diffs = self.col(i-offs) ^ self.col(i+offs+1);
+                    smudges += diffs.count_ones();
+                    if smudges > 1 {
+                        continue 'i;
+                    }
+                }
+            }
+        }
+        None
+        
+    }
+    pub fn horz_sym_smudged(&self) -> Option<usize> {
+        'i: for i in 0..(self.rows.len()-1) {
+            if nearly_eq(self.rows[i], self.rows[i+1]) {
+                let mut smudges = 0;
+                for offs in 0.. {
+                    if offs > i {
+                        if smudges == 1 {
+                            return Some(i+1);
+                        } else {
+                            continue 'i;
+                        }
+                    } else if i+offs+1 >= self.rows.len() {
+                        if smudges == 1 {
+                            return Some(i+1);
+                        } else {
+                            continue 'i;
+                        }
+                    }
+                    let diffs = self.rows[i-offs] ^ self.rows[i+offs+1];
+                    smudges += diffs.count_ones();
+                    if smudges > 1 {
                         continue 'i;
                     }
                 }
@@ -94,7 +157,16 @@ fn part1(data: &Data) -> usize {
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
-    unimplemented!()
+    let mut result = 0;
+    for field in data {
+        if let Some(n) = field.vert_sym_smudged() {
+            result += n;
+        }
+        if let Some(n) = field.horz_sym_smudged() {
+            result += 100 * n;
+        }
+    }
+    result
 }}
 
 #[test]
@@ -117,7 +189,7 @@ fn test() {
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 405);
-//    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 400);
 }
 
 fn main() -> std::io::Result<()>{
