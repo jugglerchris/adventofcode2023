faf012f67dafc7b6c79793d6a5dbdfae840f1609
Chris Emerson <github@mail.nosreme.org>
2023-12-15 05:53:59 +0000

    Day 15 part 1.

--- examples/day15.rs
+++ examples/day15.rs
@@ -1,13 +1,26 @@
 #[allow(unused)]
 use adventofcode2023::{get_input,parse_lines,regex_parser,timeit};
 
-type Data = ();
-fn parse_input(input: &str) -> Data {
+type Data<'a> = Vec<&'a [u8]>;
+fn parse_input(input: &str) -> Data<'_> {
+    input.trim().as_bytes().split(|&b| b == b',').collect()
+}
+
+fn hash(s: &[u8]) -> u8 {
+    let mut result = 0u16;
+    for &b in s {
+        result += b as u16;
+        result *= 17;
+        result &= 0xff;
+    }
+    result as u8
 }
 
 timeit!{
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    data.iter()
+        .map(|s| hash(s) as usize)
+        .sum()
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
@@ -16,11 +29,12 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
+    assert_eq!(hash(b"HASH"), 52);
+    let tests = r#"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"#;
     let data = parse_input(&tests);
 
-    assert_eq!(part1(&data), 0);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part1(&data), 1320);
+//    assert_eq!(part2(&data), 0);
 }
 
 fn main() -> std::io::Result<()>{
