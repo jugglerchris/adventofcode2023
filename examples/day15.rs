3da0cc6d2784f1121db15533f5c38502eda7dcff
Chris Emerson <github@mail.nosreme.org>
2023-12-15 06:30:13 +0000

    Day 15 part 2.

--- examples/day15.rs
+++ examples/day15.rs
@@ -1,9 +1,10 @@
+
 #[allow(unused)]
 use adventofcode2023::{get_input,parse_lines,regex_parser,timeit};
 
-type Data<'a> = Vec<&'a [u8]>;
+type Data<'a> = Vec<&'a str>;
 fn parse_input(input: &str) -> Data<'_> {
-    input.trim().as_bytes().split(|&b| b == b',').collect()
+    input.trim().split(',').collect()
 }
 
 fn hash(s: &[u8]) -> u8 {
@@ -19,12 +20,57 @@ fn hash(s: &[u8]) -> u8 {
 timeit!{
 fn part1(data: &Data) -> usize {
     data.iter()
-        .map(|s| hash(s) as usize)
+        .map(|s| hash(s.as_bytes()) as usize)
         .sum()
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
-    unimplemented!()
+    let mut boxes: Vec<Vec<(&str, usize)>> = Vec::new();
+    for _ in 0..256 {
+        boxes.push(Vec::new());
+    }
+
+    for insn in data {
+        if insn.ends_with('-') {
+            // Remove a lens
+            let name = &insn[0..(insn.len() - 1)];
+            let boxnum = hash(name.as_bytes()) as usize;
+            let bx = &mut boxes[boxnum];
+            let mut idx = None; 
+            for (i, (n, _v)) in bx.iter().enumerate() {
+                if *n == name {
+                    idx = Some(i);
+                    break;
+                }
+            }
+            if let Some(idx) = idx {
+                bx.remove(idx);
+            }
+        } else {
+            let (name, v) = insn.split_once('=').unwrap();
+            let boxnum = hash(name.as_bytes()) as usize;
+            let pow = v.parse().unwrap();
+            let bx = &mut boxes[boxnum];
+            let mut found = false;
+            for (n, v) in bx.iter_mut() {
+                if *n == name {
+                    *v = pow;
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                bx.push((name, pow));
+            }
+        }
+    }
+    let mut sum = 0;
+    for i in 0..256 {
+        for (j, v) in boxes[i].iter().enumerate() {
+            sum += (i+1) * (j+1) * v.1;
+        }
+    }
+    sum
 }}
 
 #[test]
@@ -34,7 +80,7 @@ fn test() {
     let data = parse_input(&tests);
 
     assert_eq!(part1(&data), 1320);
-//    assert_eq!(part2(&data), 0);
+    assert_eq!(part2(&data), 145);
 }
 
 fn main() -> std::io::Result<()>{
