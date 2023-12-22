528bf977b63d8a92373d79b3539e4bd2a38bebe6
Chris Emerson <github@mail.nosreme.org>
2023-12-22 07:16:04 +0000

    Day 22 part 1.

--- examples/day22.rs
+++ examples/day22.rs
@@ -1,13 +1,108 @@
+use std::collections::{HashSet, HashMap};
+
 #[allow(unused)]
 use adventofcode2023::{get_input,parse_lines,regex_parser,timeit};
 
-type Data = ();
+type Point = (usize, usize, usize);
+
+#[derive(Copy, Clone, Debug)]
+struct Brick {
+    pos0: Point,
+    pos1: Point,
+}
+impl Brick {
+    fn poses(&self) -> impl IntoIterator<Item = Point> {
+        if self.pos0.0 != self.pos1.0 {
+            let mn = self.pos0.0.min(self.pos1.0);
+            let mx = self.pos0.0.max(self.pos1.0);
+            let y = self.pos0.1;
+            let z = self.pos0.2;
+            (mn..=mx)
+                .map(|x| (x, y, z))
+                .collect::<Vec<_>>()
+        } else if self.pos0.1 != self.pos1.1 {
+            let mn = self.pos0.1.min(self.pos1.1);
+            let mx = self.pos0.1.max(self.pos1.1);
+            let x = self.pos0.0;
+            let z = self.pos0.2;
+            (mn..=mx)
+                .map(|y| (x, y, z))
+                .collect::<Vec<_>>()
+        } else {
+            let mn = self.pos0.2.min(self.pos1.2);
+            let mx = self.pos0.2.max(self.pos1.2);
+            let x = self.pos0.0;
+            let y = self.pos0.1;
+            (mn..=mx)
+                .map(|z| (x, y, z))
+                .collect::<Vec<_>>()
+        }
+    }
+}
+
+struct Data {
+    bricks: Vec<Brick>,
+}
+
+regex_parser!(parse_brick: Brick {
+    B = r#"^(\d+),(\d+),(\d+)~(\d+),(\d+),(\d+)$"# =>
+        | x1: usize, y1: usize, z1: usize, x2: usize, y2: usize, z2 : usize|
+            Brick {
+                pos0: (x1, y1, z1),
+                pos1: (x2, y2, z2)
+            }
+});
+
 fn parse_input(input: &str) -> Data {
+    Data {
+        bricks: parse_lines(input)
+    }
+}
+
+fn do_part1(data: &Data) -> usize {
+    let mut bricks = data.bricks.clone();
+    bricks.sort_by_key(|brick| brick.pos0.2.min(brick.pos1.2));
+
+    let mut unsafe_bricks = HashSet::new();
+
+    let mut space = HashMap::new();
+    for (i, brick) in bricks.iter().enumerate() {
+        let brick_z = brick.pos0.2.min(brick.pos1.2);
+        let mut drop_by = 1;
+        while brick_z > drop_by && can_drop(brick, &space, drop_by) {
+            drop_by += 1;
+        }
+        drop_by -= 1;
+
+        // Put the brick in place
+        let mut resting_on = HashSet::new();
+        for pos in brick.poses() {
+            space.insert((pos.0, pos.1, pos.2 - drop_by), i);
+            if let Some(blockno) = space.get(&(pos.0, pos.1, pos.2-drop_by-1)) {
+                if *blockno != i {
+                    resting_on.insert(*blockno);
+                }
+            }
+        }
+        if resting_on.len() == 1 {
+            unsafe_bricks.insert(resting_on.into_iter().next().unwrap());
+        }
+    }
+    bricks.len() - unsafe_bricks.len()
+}
+
+fn can_drop(brick: &Brick, space: &HashMap<Point, usize>, drop_by: usize) -> bool {
+    for point in brick.poses() {
+        if space.contains_key(&(point.0, point.1, point.2 - drop_by)) {
+            return false;
+        }
+    }
+    true
 }
 
 timeit!{
 fn part1(data: &Data) -> usize {
-    unimplemented!()
+    do_part1(data)
 }}
 timeit!{
 fn part2(data: &Data) -> usize {
@@ -16,11 +111,17 @@ fn part2(data: &Data) -> usize {
 
 #[test]
 fn test() {
-    let tests = r#""#;
+    let tests = r#"1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9"#;
     let data = parse_input(&tests);
 
-    assert_eq!(part1(&data), 0);
-    assert_eq!(part2(&data), 0);
+    assert_eq!(part1(&data), 5);
+//    assert_eq!(part2(&data), 0);
 }
 
 fn main() -> std::io::Result<()>{
