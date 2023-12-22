52e5c1eebcb3a4453fead08e2a7e176aa9cdc2f7
Chris Emerson <github@mail.nosreme.org>
2023-12-22 06:42:30 +0000

    Add day 22 input

---
+++ examples/day22.rs
@@ -0,0 +1,38 @@
+#[allow(unused)]
+use adventofcode2023::{get_input,parse_lines,regex_parser,timeit};
+
+type Data = ();
+fn parse_input(input: &str) -> Data {
+}
+
+timeit!{
+fn part1(data: &Data) -> usize {
+    unimplemented!()
+}}
+timeit!{
+fn part2(data: &Data) -> usize {
+    unimplemented!()
+}}
+
+#[test]
+fn test() {
+    let tests = r#""#;
+    let data = parse_input(&tests);
+
+    assert_eq!(part1(&data), 0);
+    assert_eq!(part2(&data), 0);
+}
+
+fn main() -> std::io::Result<()>{
+    let input = get_input(22)?;
+
+    let data = parse_input(&input);
+
+    // Part 1
+    println!("{}", part1(&data));
+
+    // Part 2
+    println!("{}", part2(&data));
+
+    Ok(())
+}
